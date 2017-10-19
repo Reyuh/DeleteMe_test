d0fcd971d6b96bcf20409fc96582b6e08fba6634
Reyuh <mr.reyuh@mail.ru>
2017-10-20 05:07:45 +0600

    8.28 with unnamed direction

--- 8.__/main.cpp
+++ 8.__/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	FILE *file;
+	char *fname = "main.cpp";
+	char result_string[100];
+
+	file = fopen(fname,"r");
+
+	if(file == NULL)
+	{
+		printf("cant open a file '%s'",fname);
+		return 0;
+	}
+
+
+	while ( fgets ( result_string , sizeof (result_string) , file ) )
+	{   bool test = true;
+	    for  ( int i = 0 ; i < 60 ; i ++ )
+        {
+            if ( result_string[i] == '\n' )
+            {
+                test = false;
+            }
+        }
+        if (test)
+        {
+            cout << result_string ;
+	    }
+	}
+
 }
