65c7d4a2170c1f513539c72584bddd60499de274
Reyuh <32583543+Reyuh@users.noreply.github.com>
2018-11-26 15:19:19 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,100 @@
+//#include "parse.h"
 #include <iostream>
+#include <fstream>
+#include <regex>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+enum class errors {error_entering_function = 0, error_entering_action = 1};
 
-int main() {
-string a;
-char b1,b2;
-getline (cin, a);
-
-for (int i=0; i<a.length(); i++)
+/*class Enter_func_exeption: public exception
 {
-	char buf = char(a[i]);
-	if(buf != ' ')
-	{
-		b1=buf;
-		break;
-	}
-}
+  virtual const char* what() const noexcept
+  {
+    return "something went wrong with enter string at function part";
+  }
+};*/
 
-for (int j=a.length()-1; j>0 ; j--)
+
+
+struct Params
 {
-	char buf = char(a[j]);
-	if(buf != ' ')
-	{
-		b2=buf;
-		break;
-	}
+    string action, body_funct;
+    vector<string> variables;
+    map<string, std::vector<double>> mymap;
+};
+
+Params parse(string exemp)
+{
+    Params obj;
+
+    //string action, body_funct;
+   // vector<string> variables;
+   // map<string, std::vector<double>> mymap;
+
+    std::smatch m;
+
+        if (std::regex_search(exemp, m, std::regex("^(\\w+)[^\\s]") )) {
+            obj.action = m[0];
+        } else {
+        throw errors::error_entering_action;
+        }
+
+
+    if (std::regex_search(exemp, m, std::regex("f:(.+)=>"))  ) {
+        string vars = m[1];
+        size_t start_pos = 0;
+        size_t pos;
+
+        while ((pos = vars.find(",", start_pos)) != string::npos) {
+            obj.variables.push_back(vars.substr(start_pos, pos - start_pos));
+            start_pos = pos + 1;
+
+        }
+        obj.variables.push_back(vars.substr(start_pos));
+
+
+    }
+
+    if (std::regex_search(exemp, m, std::regex("=>([^;]+)")) ) {
+        obj.body_funct = m[1];
+    } else {
+      throw errors::error_entering_function;
+    }
+
+    if (std::regex_search(exemp, m, std::regex("-(\\S[^\\(]+)[\\(]([^\\)]+)\\)")) ) {
+
+        string elem = m[2];
+        size_t start_key = 0;
+        size_t pos_key;
+        vector<double> key;
+
+        while ((pos_key = elem.find(",", start_key)) != string::npos) {
+            ///key.push_back(elem.substr(start_key, pos_key - start_key));
+            key.push_back(stod(elem.substr(start_key, pos_key - start_key)));
+            start_key = pos_key + 1;
+
+        }
+        key.push_back(stod(elem.substr(start_key)));
+
+        obj.mymap[m[1]] = key;
+
+
+
+    }
+    return obj;
 }
 
-cout << "\n" << b1 << "\n" << b2;
-if (b1=b2){
-	cout << "first and last elem are equal";
-}
+
+int main()
+{
+    string exemp = "calc f:x, y =>x*x - 5*y + z; -args(5 ,4 );";
+
+
+
+    Params test = parse(exemp);
+
+    cout << test.action ;
 }
