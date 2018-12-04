6e0821794cbe1bd63cb8bc243b7c195ceac82038
Reyuh <32583543+Reyuh@users.noreply.github.com>
2018-12-04 09:55:45 +0600

    Add files via upload
    1.4.
    Zeidel

--- main.cpp
+++ main.cpp
@@ -1,100 +1,89 @@
-//#include "parse.h"
 #include <iostream>
-#include <fstream>
-#include <regex>
-#include <string>
-#include <vector>
+#include <cmath>
 
 using namespace std;
 
-enum class errors {error_entering_function = 0, error_entering_action = 1};
-
-/*class Enter_func_exeption: public exception
+double okr(double X, double eps)
 {
-  virtual const char* what() const noexcept
-  {
-    return "something went wrong with enter string at function part";
-  }
-};*/
+	int i = 0;
+	while (eps != 1)
+	{
+		i++;
+		eps *= 10;
+	}
+	int okr = pow(double(10),i);
+	X = int(X * okr + 0.5) / double(okr);
 
-
-
-struct Params
-{
-    string action, body_funct;
-    vector<string> variables;
-    map<string, std::vector<double>> mymap;
-};
-
-Params parse(string exemp)
-{
-    Params obj;
-
-    //string action, body_funct;
-   // vector<string> variables;
-   // map<string, std::vector<double>> mymap;
-
-    std::smatch m;
-
-        if (std::regex_search(exemp, m, std::regex("^(\\w+)[^\\s]") )) {
-            obj.action = m[0];
-        } else {
-        throw errors::error_entering_action;
-        }
-
-
-    if (std::regex_search(exemp, m, std::regex("f:(.+)=>"))  ) {
-        string vars = m[1];
-        size_t start_pos = 0;
-        size_t pos;
-
-        while ((pos = vars.find(",", start_pos)) != string::npos) {
-            obj.variables.push_back(vars.substr(start_pos, pos - start_pos));
-            start_pos = pos + 1;
-
-        }
-        obj.variables.push_back(vars.substr(start_pos));
-
-
-    }
-
-    if (std::regex_search(exemp, m, std::regex("=>([^;]+)")) ) {
-        obj.body_funct = m[1];
-    } else {
-      throw errors::error_entering_function;
-    }
-
-    if (std::regex_search(exemp, m, std::regex("-(\\S[^\\(]+)[\\(]([^\\)]+)\\)")) ) {
-
-        string elem = m[2];
-        size_t start_key = 0;
-        size_t pos_key;
-        vector<double> key;
-
-        while ((pos_key = elem.find(",", start_key)) != string::npos) {
-            ///key.push_back(elem.substr(start_key, pos_key - start_key));
-            key.push_back(stod(elem.substr(start_key, pos_key - start_key)));
-            start_key = pos_key + 1;
-
-        }
-        key.push_back(stod(elem.substr(start_key)));
-
-        obj.mymap[m[1]] = key;
-
-
-
-    }
-    return obj;
+	return X;
 }
 
+void iterat(double A[10][10], double B[10], int N, double eps)
+{
+
+
+		int k = 0,i,j;
+		double X[10], Y[10], g;
+		for(i = 0; i < N; i++)
+			X[i] = B[i];
+
+
+			{
+				for (i = 0; i < N; i++)
+				{
+					g = B[i];
+					for (j = 0; j < N; j++)
+                        k ++;
+						g = g + A[i][j] * X[j];
+					X[i] = g;
+				}
+
+			}
+
+			cout << "Решение системы:" << endl << endl;
+			for (i = 0; i < N; i++)
+				cout << "X"<< i <<" = "<< okr(X[i],eps) <<"" << endl;
+			cout << endl << endl;
+			cout << "число итераций" << k << endl;
+
+
+}
 
 int main()
 {
-    string exemp = "calc f:x, y =>x*x - 5*y + z; -args(5 ,4 );";
+	double eps, A[10][10], B[10];
+	int N, i, j;
+	cout << "Введите размер квадратной матрицы: ";
+	cin >> N;
+	cout << "Введите точность вычислений: ";
+	cin >> eps;
+	cout << "Заполните матрицу А: " << endl << endl;
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+		{
+			cout << "A[" << i << "][" << j << "] = ";
+			cin >> A[i][j];
+		}
+	cout << endl << endl;
+		cout << "Ваша матрица А: " << endl << endl;
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+				cout << A[i][j] << " ";
+		cout << endl;
+		}
+
+		cout << endl;
+
+		cout << "Заполните столбец свободных членов: " << endl << endl;
+		for (i = 0; i < N; i++)
+		{
+			cout << "В[" << i+1 << "] = ";
+			cin >> B [i];
+		}
+
+		iterat(A,B,N,eps);
+
+		return 0;
 
 
-
-    Params test = parse(exemp);
-
-    cout << test.action ;
 }
