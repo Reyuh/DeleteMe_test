28c71758c024a6b7a8f7782ad3a67ef997a324f1
Reyuh <32583543+Reyuh@users.noreply.github.com>
2018-12-11 02:24:41 +0600

    2.2
    Wow, it must be worked

---
+++ 2,2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<cmath>
+#include <math.h>
+using namespace std;
+double f(double x)
+{
+//return (sqrt(3-exp(-x)*2/x)); 1.6....
+//return (log((x*x*x-3*x)/2)); nan
+return (pow( (3*x+2*exp(-x)),0.33333333333333333333333) ) ;
+}
+
+int main()
+{
+    double y,x0,x;
+    double e=0.000000000001;
+    int n=0;
+    cout<<"enter x0:";
+    cin>>x0;
+
+     x=x0;
+     do
+      {
+         x0=x;
+         x=f(x0);
+         n++;
+         cout<<"x"<<n<<"="<<x<<endl;
+     } while(fabs(x-x0)>e);
+
+    cout<<endl<<"root: "<<x<<endl;
+
+
+    y=(x*x*x-3*x-2*exp(-x));
+    cout<<"test: "<<y;
+}
